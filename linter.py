1651fe021523f7dbb06b6c8c7e22b5c68703a05c
herr kaste <herr.kaste@gmail.com>
2018-05-11 18:14:23 +0200

    Implement ignoring auto-fixable codes
    Fixes #65

--- linter.py
+++ linter.py
@@ -10,7 +10,9 @@ class Flake8(PythonLinter):
 
     cmd = ('flake8', '--format', 'default', '${args}', '-')
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.python',
+        # By default, filter codes Sublime can auto-fix
+        'ignore-fixables': True
     }
 
     # The following regex marks these pyflakes and pep8 codes as errors.
@@ -39,6 +41,33 @@ class Flake8(PythonLinter):
         r'(?P<message>.*)'
     )
     multiline = True
+
+    def parse_output(self, proc, virtual_view):
+        settings = self.get_view_settings()
+        errors = super().parse_output(proc, virtual_view)
+
+        if not settings.get('ignore-fixables', False):
+            return errors
+
+        filtered_errors = []
+        for error in errors:
+            code = error['code']
+            if code in ('W291', 'W293'):  # no 'trailing' WS errors
+                continue
+
+            if code == 'W391':
+                # Fixable if one WS line is at EOF, except the view only has
+                # one line.
+                lines = len(virtual_view._newlines) - 1
+                if (
+                    virtual_view.select_line(lines - 1).strip() == '' and
+                    (lines < 2 or virtual_view.select_line(lines - 2).strip() != '')
+                ):
+                    continue
+
+            filtered_errors.append(error)
+
+        return filtered_errors
 
     def reposition_match(self, line, col, m, virtual_view):
         """Reposition white-space errors."""
