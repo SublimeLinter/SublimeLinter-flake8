5bc4a764058f604d7dda854266ef98351b01e2b5
Ruben <15186467+ROpdebee@users.noreply.github.com>
2019-06-16 14:20:48 +0200

    Allow colon after error code
    Compatibility with flake8-pie

--- linter.py
+++ linter.py
@@ -43,7 +43,7 @@ class Flake8(PythonLinter):
     regex = (
         r'^.+?:(?P<line>\d+):(?P<col>\d+): '
         r'(?:(?P<error>(?:F(?:40[24]|8(?:12|2[123]|31))|E(?:11[23]|90[12]|999)))|'
-        r'(?P<warning>\w+\d+)) '
+        r'(?P<warning>\w+\d+):?) '
         r'(?P<message>.*)'
     )
     multiline = True
